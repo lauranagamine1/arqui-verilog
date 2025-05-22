0d32cc4669e3aacbb75dc89c14c2cd8ca35bb2d8
Cesar Carranza <166084252+Chimichami@users.noreply.github.com>
2025-05-22 14:09:13 -0500

    Update string_to_lower.asm

--- s9-challenge-isa/string_to_lower.asm
+++ s9-challenge-isa/string_to_lower.asm
@@ -11,16 +11,16 @@ _start:
 	CMP R2, #0x00
 	BEQ end //Si es el ultimo elemento, acaba el programa
 	
-	CMP R2, #0x41
+	CMP R2, #0x41 //si es menor a "A", no lo cambia
 	BLT notupper
-	CMP R2, #0x5A
+	CMP R2, #0x5A // si es mayor a "Z", no lo cambia
 	BGT notupper
 	
-	ADD R2, R2, #0x20
+	ADD R2, R2, #0x20 //transmoración a minúscula
 	STRB R2, [R0,R1]
 	
 	notupper:
-	ADD R1, R1, #1
+	ADD R1, R1, #1 // i = i+1
 	B for
 	end:
 	MOV PC, LR
