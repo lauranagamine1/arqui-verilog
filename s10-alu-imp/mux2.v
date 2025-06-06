32bff48ac43a76c0cfb7443b0ed1d6fba0f282c9
Cesar Carranza <166084252+Chimichami@users.noreply.github.com>
2025-06-05 21:14:41 -0500

    Update mux2.v

--- s10-alu-imp/mux2.v
+++ s10-alu-imp/mux2.v
@@ -1,4 +1,4 @@
 `timescale 1ns / 1ps
-module mux2([4:0] input a, [4:0] input b, input s, [4:0] y);
-  assign y = (~s and a) or (s and b);
+module mux2(input [4:0] a, input [4:0] b, input s, output [4:0] y);
+  assign y = (~s & a) | (s & b);
 endmodule
