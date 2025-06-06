4f3d620eca731204e464c5e745e759d4b45d9f18
Cesar Carranza <166084252+Chimichami@users.noreply.github.com>
2025-06-06 13:29:30 -0500

    Update top.v

--- s10-alu-imp/top.v
+++ s10-alu-imp/top.v
@@ -25,15 +25,9 @@ module top(input[4:0] a,b,input [2:0] ALUControl, input [1:0] bshift, input sele
     wire [4:0] w1;
     wire [4:0] w2;
     wire [4:0] w3;
-    wire [4:0] w4;
     
     shift_operator_l s1(.A(a), .bshift(bshift), .y(w1));
     shift_operator_r s2(.A(a), .bshift(bshift), .y(w2));
     mux2 s3(.a(w1), .b(w2), .s(select), .y(w3));
-  alu_xor ut(.a(w3), .b(b), .ALUControl(ALUControl), .Result(w4), .ALUFlags(ALUFlags));
-  convertor waza(.resultado(w4), .converted(Result));
-    
-    
-       
-    
+  alu_xor ut(.a(w3), .b(b), .ALUControl(ALUControl), .Result(Result), .ALUFlags(ALUFlags));
 endmodule
