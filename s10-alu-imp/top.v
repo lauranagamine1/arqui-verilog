95c195d134031f4d54e91a1d767c2d7d87592d50
Cesar Carranza <166084252+Chimichami@users.noreply.github.com>
2025-06-05 19:49:42 -0500

    Update top.v

--- s10-alu-imp/top.v
+++ s10-alu-imp/top.v
@@ -20,12 +20,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module top(input[4:0] a,b,input [2:0] ALUControl, input [1:0] bshift, output [4:0] Result,
+module top(input[4:0] a,b,input [2:0] ALUControl, input [1:0] bshift, input select, output [4:0] Result,
     output wire [3:0] ALUFlags);
     wire [4:0] w1;
+    wire [4:0] w2;
+    wire [4:0] w3;
     
-    shift_operator s(.A(a), .bshift(bshift), .y(w1));
-    alu_xor ut(.a(w1), .b(b), .ALUControl(ALUControl), .Result(Result), .ALUFlags(ALUFlags));
+    shift_operator_l s(.A(a), .bshift(bshift), .y(w1));
+    shift_operator_r s(.A(a), .bshift(bshift), .y(w2));
+    mux2 s(.a(w1), .b(w2), .s(select), .y(w3));
+    alu_xor ut(.a(w3), .b(b), .ALUControl(ALUControl), .Result(Result), .ALUFlags(ALUFlags));
     
        
     
