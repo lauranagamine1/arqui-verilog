a796327d62210715ee34211d3b32f2a2648b59d1
lauranagamine1 <laura.nagamine@utec.edu.pe>
2025-06-11 03:25:52 -0500

    feat: EOR single cycle

--- s12-sing-cyc-plantilla/imem.v
+++ s12-sing-cyc-plantilla/imem.v
@@ -1,11 +1,11 @@
-`timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
 // 
-// Create Date: 06/04/2025 02:11:36 PM
+// Create Date: 06/04/2025 02:27:03 PM
 // Design Name: 
-// Module Name: arm_single
+// Module Name: imem
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,7 +20,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module arm_single(
-
-    );
+module imem (
+	a,
+	rd
+);
+	input wire [31:0] a;
+	output wire [31:0] rd;
+	reg [31:0] RAM [63:0]; // antes: wire [31:0] RAM [63:0]
+	initial begin 
+	$readmemh("memfile.mem", RAM); // antes. memfile.dat
+	end
+	
+	assign rd = RAM[a[31:2]];
 endmodule
