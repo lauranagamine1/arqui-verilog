9aaa15b928f0320296dc8e7267eef544460c000f
lauranagamine1 <laura.nagamine@utec.edu.pe>
2025-06-17 17:59:15 -0500

    add: challenge single cycle

---
+++ s12-challenge-single-cycle/controller.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/04/2025 02:25:49 PM
+// Design Name: 
+// Module Name: controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module controller (
+	clk,
+	reset,
+	Instr,
+	ALUFlags,
+	RegSrc,
+	RegWrite,
+	ImmSrc,
+	ALUSrc,
+	ALUControl,
+	MemWrite,
+	MemtoReg,
+	PCSrc
+);
+	input wire clk;
+	input wire reset;
+	input wire [31:12] Instr;
+	input wire [3:0] ALUFlags;
+	output wire [1:0] RegSrc;
+	output wire RegWrite;
+	output wire [1:0] ImmSrc;
+	output wire ALUSrc;
+	output wire [2:0] ALUControl;
+	output wire MemWrite;
+	output wire MemtoReg;
+	output wire PCSrc;
+	wire [1:0] FlagW;
+	wire PCS;
+	wire RegW;
+	wire MemW;
+	decode dec(
+		.Op(Instr[27:26]),
+		.Funct(Instr[25:20]),
+		.Rd(Instr[15:12]),
+		.FlagW(FlagW),
+		.PCS(PCS),
+		.RegW(RegW),
+		.MemW(MemW),
+		.MemtoReg(MemtoReg),
+		.ALUSrc(ALUSrc),
+		.ImmSrc(ImmSrc),
+		.RegSrc(RegSrc),
+		.ALUControl(ALUControl)
+	);
+	condlogic cl(
+		.clk(clk),
+		.reset(reset),
+		.Cond(Instr[31:28]),
+		.ALUFlags(ALUFlags),
+		.FlagW(FlagW),
+		.PCS(PCS),
+		.RegW(RegW),
+		.MemW(MemW),
+		.PCSrc(PCSrc),
+		.RegWrite(RegWrite),
+		.MemWrite(MemWrite)
+	);
+endmodule
+
+
+
